629aacf995078e4760f818da2e397a4261761137
Igor <igordossantoslopes73@gmail.com>
2026-03-12 11:48:23 -0300

    Primeiro commit

---
+++ descarte-ideal/src/main/java/univille/fso/descarte_ideal/DescarteIdealApplication.java
@@ -0,0 +1,13 @@
+package univille.fso.descarte_ideal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescarteIdealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescarteIdealApplication.class, args);
+	}
+
+}
---
+++ descarte-ideal/src/test/java/univille/fso/descarte_ideal/DescarteIdealApplicationTests.java
@@ -0,0 +1,13 @@
+package univille.fso.descarte_ideal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescarteIdealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
